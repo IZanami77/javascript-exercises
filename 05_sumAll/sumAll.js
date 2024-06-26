896b9be180a66eb4f1d32fb25edcbd3abf10a7ac
IZanami77 <iZanamiNoOkami7@gmail.com>
2024-06-26 17:47:40 -0600

    Submitted the sumAll problem

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(numStart,numFinal) {
 
+    if (!Number.isInteger(numStart) || !Number.isInteger(numFinal)) return "ERROR";
+    if (numStart < 0 || numFinal < 0) return "ERROR";
+    if (numStart > numFinal){
+        const temp = numStart;
+        numStart = numFinal;
+        numFinal = temp;
+    }
+    let totalSum = 0;
+
+    do{
+        totalSum += numStart;
+        numStart++;
+    }while(numFinal >= numStart);
+    
+
+
+    return totalSum;
+   
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+// let totalSum = 0;
+// do{
+//     totalSum += numStart;
+//     numStart++;
+// }while(numFinal >= numStart)
+// return totalSum;
