5253d7b310d2b7ae0e9727e4659a22ee9879a37f
IZanami77 <iZanamiNoOkami7@gmail.com>
2024-06-26 12:53:08 -0600

    Submitted the fourth solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementToRemoved) {
+    newArray = [];
+    array.forEach((element) => {
+
+        if(!elementToRemoved.includes(element)){
+            newArray.push(element);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
